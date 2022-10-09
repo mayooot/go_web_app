7502be5c11ece24a2c59b4559ba1c5fc70bd3f45
Bertram Li <1129972602@qq.com>
2022-10-09 17:47:13 +0800

    frist commit

---
+++ routes/routes.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"web_app/logger"
+	"web_app/settings"
+)
+
+func Setup() *gin.Engine {
+	r := gin.New()
+	r.Use(logger.GinLogger(), logger.GinRecovery(true))
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Ok")
+	})
+	r.GET("/version", func(c *gin.Context) {
+		c.String(http.StatusOK, settings.Conf.Version)
+	})
+	return r
+}
